770ecaabb20e66310e669bd242e33e690ca34486
Adam <thundercato@users.noreply.github.com>
2026-08-02 10:53:12 +0100

    build: add Vite configuration

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
+
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? './' : '/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+}));
